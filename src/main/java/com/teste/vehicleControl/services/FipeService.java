30ea10c512fe79244c239558e91412ca16ea3f47
gonLeo <leonardo.oliveira@totemti.com.br>
2021-06-28 21:18:16 -0400

    Using regex expression to filter returns

--- src/main/java/com/teste/vehicleControl/services/FipeService.java
+++ src/main/java/com/teste/vehicleControl/services/FipeService.java
@@ -1,6 +1,7 @@
 package com.teste.vehicleControl.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -31,11 +32,34 @@ public class FipeService {
 
     public String searchVehicleValue(@Valid VehicleParamDto request) {
         long brandCode = findBrandCode(request.getBrand());
+        if (notFound(brandCode)){
+            return "";
+        };
         long modelCode = findModelCode(brandCode, request.getModel());
+        if (notFound(modelCode)){
+            return "";
+        };
         String codeYear = findYearCode(brandCode, modelCode, request.getYear());
+        if (notFoundYear(codeYear)){
+            return "";
+        };
         CarDto car = repository.getCar(brandCode, modelCode, codeYear);
         return car.getValue();
     }        
+
+    private boolean notFoundYear(String yearCode) {
+        if(yearCode == ""){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean notFound(long code) {
+        if(code == -1){
+            return true;
+        }
+        return false;
+    }
 
     private long findBrandCode(String brand) {
         List<ResponseApiDto> brands = repository.getAllBrands();        
@@ -48,14 +72,20 @@ public class FipeService {
     }
 
     private String findYearCode(long brandCode, long modelCode, String year) {
+        String regexName = ".*" + year.toLowerCase() + ".*";
         List<ResponseYearDto> years = repository.getAllYear(brandCode, modelCode);
-        return years.stream().filter(obj -> obj.getName().equals(year)).findFirst().get().getCodeNumber();
+        List<ResponseYearDto> response = years.stream().filter(obj -> obj.getName().toLowerCase().matches(regexName)).collect(Collectors.toList());
+        String codeYear = !response.isEmpty() ? response.get(0).getCodeNumber() : "";
+        return codeYear;
         
     }    
 
     private long getCodeNumber(List<ResponseApiDto> responseApi, String name) {
-        ResponseApiDto response = responseApi.stream().filter(obj -> obj.getName().equals(name)).findFirst().get();
-        return response.getCodeNumber();
+        String regexName = ".*" + name.toLowerCase() + ".*";
+        List<ResponseApiDto> response = responseApi.stream().filter(obj -> obj.getName().toLowerCase().matches(regexName)).collect(Collectors.toList());
+
+        long codeNumber = !response.isEmpty() ? response.get(0).getCodeNumber() : -1;
+        return codeNumber;
     }
     
 }
